3e903b2e0215aeb64a8112763c278b98af45669d
oyo <bemyangel2@gmail.com>
2026-02-21 18:25:39 +0900

    feat: Tanstack Query 설치 및 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import './index.css'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+	<StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
+	</StrictMode>,
 )
