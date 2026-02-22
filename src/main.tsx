4f86a2a71c43e74ba02e67f159951013f8cfc825
oyo <bemyangel2@gmail.com>
2026-02-23 02:06:26 +0900

    feat: 기본 앱 구성

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
+import router from '@/router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
 import './index.css'
 
 const queryClient = new QueryClient()
@@ -9,7 +10,7 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<App />
+			<RouterProvider router={router} />
 		</QueryClientProvider>
 	</StrictMode>,
 )
